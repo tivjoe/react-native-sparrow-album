9c04705f2e9380294e8ca638dffdd3e1c5c9006e
noobjoe <hackerjoe@163.com>
2021-01-13 21:25:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { SparrowAlbum } = NativeModules;
+
+export default SparrowAlbum;
---
+++ ios/SparrowAlbum.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface SparrowAlbum : NSObject <RCTBridgeModule>
+
+@end
